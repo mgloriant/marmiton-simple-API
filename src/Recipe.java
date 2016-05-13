7900df468320644bbce3bde5e4a3f4f693395b92
Paul LESUR <paul.lesur@free.fr>
2016-05-13 16:21:03 -0400

    Javadoc about Recipe

--- src/Recipe.java
+++ src/Recipe.java
@@ -5,20 +5,49 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 
 /**
- * Created by paull on 12/02/2016.
+ * The class Recipe describe a recipe.
  */
 public class Recipe {
+    /**
+     * Name of the Recipe
+     */
     private String name;
+    /**
+     * URL link of the Recipe extracted from <a href="http://marmiton.org">http://marmiton.org</a>
+     */
     private String urlLink;
+    /**
+     * HTML page of the Recipe
+     */
     private Document page;
+    /**
+     * HTML code that contains informations about the Recipe
+     */
     private Element pageContent;
 
+    /**
+     * Preparation time of the Recipe
+     */
     private String prepTime;
+    /**
+     * Cooking time of the Recipe
+     */
     private String cookingTime;
+    /**
+     * Details about the Recipe : instructions about how to make this Recipe
+     */
     private String details;
+    /**
+     * Ingredients needed to cook the Recipe
+     */
     private String ingredients;
 
 
+    /**
+     * Simple constructor of Recipe.
+     * @param name Name of the Recipe.
+     * @param urlLink Url link of the Recipe.
+     */
     public Recipe(String name, String urlLink)  {
 
         this.name = name;
@@ -27,6 +56,10 @@ public class Recipe {
 
     }
 
+    /**
+     * Method that send a query to marmiton.org and load the informations about the Recipe.
+     * @throws IOException
+     */
     public void loadInformations()throws IOException{
         page = Jsoup.connect(this.getUrlLink()).get();
         pageContent = page.getElementsByClass("m_content_recette_main").first();
@@ -39,6 +72,7 @@ public class Recipe {
         ingredients = pageContent.getElementsByClass("m_content_recette_ingredients").first().text();
 
     }
+
 
     public String getName() {
         return name;
